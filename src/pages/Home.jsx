b4094500df7092fa4eca6273f417ac209928557d
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-03-21 22:13:29 +0300

    CSS updating
    // ONLY CSS
    
    * Backround DONE
    * Navbar DONE
    * Banner DONE
    * Photo Upload Area DONE

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,16 @@ import bgImage from "../assets/images/background-desktop.png";
 
 export default function Home() {
     return(
-        <div className="main-container w-screen h-screen font-Inconsolata flex flex-col items-center" style={{ backgroundImage: `url(${bgImage})`}}>
+        <>
+        <div className="background-container absolute -z-10 w-full h-full">
+            <div className="bg-layer absolute bg-desktop-layer-1 inset-0"></div>
+            <div className="bg-layer absolute bg-desktop-layer-2 inset-0"></div>
+            <div className="bg-layer absolute bg-desktop-layer-3 inset-0"></div>
+            <div className="bg-layer absolute bg-desktop-layer-4 inset-0"></div>
+            <div className="bg-layer absolute bg-desktop-layer-5 inset-0"></div>
+            <div className="bg-layer"></div>
+        </div>
+        <div className="main-container w-screen h-screen font-Inconsolata flex flex-col items-center">
             <Navbar />
             <div className="banner-container text-white flex-col justify-center items-center mt-15 w-1/2">
                 <div className="banner flex flex-col items-center justify-center w-full"> 
@@ -14,5 +23,6 @@ export default function Home() {
             </div>
             <TicketForm />
         </div>
+        </>
     );
 }
